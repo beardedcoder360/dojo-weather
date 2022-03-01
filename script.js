ce656eaff6a3cfa7855f722c8336e77ad0618940
beardedcoder360 <95878712+beardedcoder360@users.noreply.github.com>
2022-02-28 16:46:25 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function message() {
+  alert("Godzilla has taken over the city!!!");
+}
+function turnOff() {
+  var cook = document.getElementById("cook");
+  cook.parentNode.removeChild(cook);
+}
+function myFunction() {
+  var x = document.getElementById("Temp").value;
+  document.getElementById("deg").innerHTML = "80°";
+  document.getElementById("deg2").innerHTML = "66°";
+  document.getElementById("deg3").innerHTML = "75°";
+  document.getElementById("deg4").innerHTML = "65°";
+  document.getElementById("deg5").innerHTML = "69°";
+  document.getElementById("deg6").innerHTML = "61°";
+  document.getElementById("deg7").innerHTML = "78°";
+  document.getElementById("deg8").innerHTML = "70°";
+}
